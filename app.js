0577c0892966db0c0c91d8838f8cd3b9be2faf18
Thirumavalavan <catchgjt@gmail.com>
2016-11-16 18:33:38 +0530

    port number changed to 8080

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ res.sendFile(__dirname+'/index.html')
 app.get('/query', function(req,res){
 res.json(["iphone","samsung","LG","sony"]);
 })
-app.listen(process.env.PORT || 5000, function(){
-console.log("Server started @ 5000");
+app.listen(process.env.PORT || 8080, function(){
+console.log("Server started @ 8080");
 });
